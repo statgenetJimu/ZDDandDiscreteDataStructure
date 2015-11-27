b79590b7f48076e821fa4d4aa7a63455b978ed15
ryamada22 <ryamada@genome.med.kyoto-u.ac.jp>
2015-11-28 08:26:13 +0900

    p03.py

---
+++ python/src/p03.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+from graphillion import GraphSet
+import graphillion.tutorial as tl
+n=3
+m=2
+nm = n+m
+goal = (n+1)*(m+1)
+
+universe=tl.grid(n,m)
+GraphSet.set_universe(universe)
+
+Gt=GraphSet.paths(1,goal)
+
+Gs=Gt.graph_size(nm)
+example = Gs.choice()
+example = (Gs.including(5)).choice()
+
+print len(Gs)
+print example
+tl.draw(example)
